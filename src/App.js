275189d3b0c33d8f820376f20ceb00beb1203722
Steven Illien <steven.illien@gmail.com>
2020-06-06 17:26:17 +0200

    Handles widndow resize

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import React, { createRef, useEffect, useMemo, useState } from 'react';
 import './App.css';
 import SVGShape from './components/svg-shape';
 import { sections } from './config';
+import { useWindowSize } from './hooks/use-window-size';
 
 function App() {
   const refs= useMemo(() => sections.map(() => createRef()), []);
   const [scrollY, setScrollY] = useState(0);
   const [sectionHeights, setSectionHeights] = useState([]);
+  const [width, height] = useWindowSize();
 
   useScrollPosition(({ currPos }) => setScrollY(- currPos.y), []);
-  useEffect(() => setSectionHeights(refs.map(ref => ref.current.offsetHeight)), [refs]);
+  useEffect(() => {
+    const sectionHeights = refs.map(ref => ref.current.offsetHeight);
+    setSectionHeights(sectionHeights);
+  }, [refs, width, height]);
 
   return (
     <div>
@@ -29,8 +34,5 @@ function App() {
 }
 
 export default App;
-
-
-// TODO -> detect window resize -> recompute sectionHeights
 
 // TODO -> detect change of any section height -> recompute sectionHeights
